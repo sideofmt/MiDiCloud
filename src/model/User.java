f365f95bfbe4e4c6236ae83f4cdc4e1f62c69ca2
sideofmt <mtmt.garireo.39440@gmail.com>
2015-07-07 15:32:37 +0900

    変更されていなかったUserとUserDAOを正しいものに変更

--- src/model/User.java
+++ src/model/User.java
@@ -1,5 +1,72 @@
 package model;
 
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+
 public class User {
+	private int userID;
+	private String username;
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	private String password;
+	private String mailAddress;
+	private String profile;
+	private Byte icon;
+	private ArrayList<Integer> MIDI_IDs;
+	private ArrayList<Integer> commentIDs;
+	private boolean isManager;
+	public int getUserID() {
+		return userID;
+	}
+	public void setUserID(int userID) {
+		this.userID = userID;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getMailAddress() {
+		return mailAddress;
+	}
+	public void setMailAddress(String mailAddress) {
+		this.mailAddress = mailAddress;
+	}
+	public String getProfile() {
+		return profile;
+	}
+	public void setProfile(String profile) {
+		this.profile = profile;
+	}
+	public Byte getIcon() {
+		return icon;
+	}
+	public void setIcon(Byte icon) {
+		this.icon = icon;
+	}
+	public ArrayList<Integer> getMIDI_IDs() {
+		return MIDI_IDs;
+	}
+	public void setMIDI_IDs(ArrayList<Integer> mIDI_IDs) {
+		MIDI_IDs = mIDI_IDs;
+	}
+	public ArrayList<Integer> getCommentIDs() {
+		return commentIDs;
+	}
+	public void setCommentIDs(ArrayList<Integer> commentIDs) {
+		this.commentIDs = commentIDs;
+	}
+	public boolean isManager() {
+		return isManager;
+	}
+	public void setManager(boolean isManager) {
+		this.isManager = isManager;
+	}
+
 
 }
--- src/model/UserDAO.java
+++ src/model/UserDAO.java
@@ -1,35 +1,266 @@
 package model;
 
 import java.sql.*;
+import java.util.ArrayList;
 
 public class UserDAO {
 	final private static String dbname = "MidiCloud";
-	final private static String user = "dbpuser";
+	final private static String username = "dbpuser";
 	final private static String password = "hogehoge";
 	final private static String driverClassName = "org.postgresql.Driver";
 	final private static String url = "jdbc:postgresql://localhost/" + dbname;
 
-	public boolean check(User user)throws SQLException{
-		boolean result = false;
+	public boolean addUser(User user)throws SQLException{
 		Connection connection;
-		String sql = "select * from member where name=? and pass=?";
+		String sql = "insert into user values (?, ?, ?, ?, ?, ?, ?, ?, ?)";
 
 		try{
 			Class.forName(driverClassName);
-			connection = DriverManager.getConnection(url, user, password);
+			connection = DriverManager.getConnection(url, username, password);
 			PreparedStatement pstmt = connection.prepareStatement(sql);
 
-			pstmt.setString(1, member.getName());
-			pstmt.setString(2, member.getPass());
+			pstmt.setInt(1, user.getUserID());
+			pstmt.setString(2, user.getUsername());
+			pstmt.setString(3, user.getPassword());
+			pstmt.setString(4, user.getMailAddress());
+			pstmt.setString(5, user.getProfile());
+			pstmt.setByte(6, user.getIcon());
+			pstmt.setObject(7, user.getMIDI_IDs());
+			pstmt.setObject(8, user.getCommentIDs());
+			pstmt.setBoolean(9, user.isManager());
 
 			ResultSet resultSet = pstmt.executeQuery();
-			if(resultSet.next()) result = true;
+
+			resultSet.close();
+			connection.close();
+			return true;
+		}catch(Exception e){
+			e.printStackTrace();
+			return false;
+		}
+	}
+
+	public User returnUser(int userID)throws SQLException{
+		Connection connection;
+		User user = new User();
+		String sql = "select * from user where userID=?";
+
+		try{
+			Class.forName(driverClassName);
+			connection = DriverManager.getConnection(url, username, password);
+			PreparedStatement pstmt = connection.prepareStatement(sql);
+
+			pstmt.setInt(1, userID);
+
+			ResultSet resultSet = pstmt.executeQuery();
+
+			String userName = resultSet.getString("username");
+			String password = resultSet.getString("password");
+			String mailAddress = resultSet.getString("mailAddress");
+			String profile = resultSet.getString("profile");
+			Byte icon = resultSet.getByte("icon");
+			ArrayList<Integer> MIDI_IDs = (ArrayList<Integer>)resultSet.getObject("MIDI_IDs");
+			ArrayList<Integer> comment_IDs = (ArrayList<Integer>) resultSet.getObject("commentIDs");
+			Boolean isManagaer = resultSet.getBoolean("isManager");
+
+			user.setUserID(userID);
+			user.setUsername(userName);
+			user.setPassword(password);
+			user.setMailAddress(mailAddress);
+			user.setProfile(profile);
+			user.setIcon(icon);
+			user.setMIDI_IDs(MIDI_IDs);
+			user.setCommentIDs(comment_IDs);
+			user.setManager(isManagaer);
+
+			resultSet.close();
+			connection.close();
+
+			return user;
+
+		}catch(Exception e){
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+
+	public ArrayList<User> returnUser(ArrayList<Integer> userIDs)throws SQLException{
+		Connection connection;
+		User user = new User();
+		String sql = "select * from user where userID=?";
+		ArrayList<User> USERs = new ArrayList<User>();
+
+		try{
+			Class.forName(driverClassName);
+			connection = DriverManager.getConnection(url, username, password);
+
+			for(Integer userID : userIDs){
+
+				PreparedStatement pstmt = connection.prepareStatement(sql);
+				pstmt.setInt(1, userID);
+				ResultSet resultSet = pstmt.executeQuery();
+
+				String userName = resultSet.getString("username");
+				String password = resultSet.getString("password");
+				String mailAddress = resultSet.getString("mailAddress");
+				String profile = resultSet.getString("profile");
+				Byte icon = resultSet.getByte("icon");
+				ArrayList<Integer> MIDI_IDs = (ArrayList<Integer>)resultSet.getObject("MIDI_IDs");
+				ArrayList<Integer> comment_IDs = (ArrayList<Integer>) resultSet.getObject("commentIDs");
+				Boolean isManagaer = resultSet.getBoolean("isManager");
+
+				user.setUserID(userID);
+				user.setUsername(userName);
+				user.setPassword(password);
+				user.setMailAddress(mailAddress);
+				user.setProfile(profile);
+				user.setIcon(icon);
+				user.setMIDI_IDs(MIDI_IDs);
+				user.setCommentIDs(comment_IDs);
+				user.setManager(isManagaer);
+
+				USERs.add(user);
+
+				resultSet.close();
+			}
+
+			connection.close();
+			return USERs;
+
+		}catch(Exception e){
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+
+	public void updateUser(User user)throws SQLException{
+		Connection connection;
+		String sql = "update user set username=?,password=?,mailAddress=?,profile=?,icon=?,MIDI_IDs=?,commentIDs=?,isManagaer=? where userID=?";
+
+		try{
+			Class.forName(driverClassName);
+			connection = DriverManager.getConnection(url, username, password);
+			PreparedStatement pstmt = connection.prepareStatement(sql);
+
+
+			pstmt.setString(1, user.getUsername());
+			pstmt.setString(2, user.getPassword());
+			pstmt.setString(3, user.getMailAddress());
+			pstmt.setString(4, user.getProfile());
+			pstmt.setByte(5, user.getIcon());
+			pstmt.setObject(6, user.getMIDI_IDs());
+			pstmt.setObject(7, user.getCommentIDs());
+			pstmt.setBoolean(8, user.isManager());
+			pstmt.setInt(9, user.getUserID());
+
+			ResultSet resultSet = pstmt.executeQuery();
 
 			resultSet.close();
 			connection.close();
 		}catch(Exception e){
 			e.printStackTrace();
 		}
-		return result;
 	}
+
+	public void deleteUser(int userID)throws SQLException{
+		Connection connection;
+		String sql = "delete from user where userID=?";
+
+		try{
+			Class.forName(driverClassName);
+			connection = DriverManager.getConnection(url, username, password);
+			PreparedStatement pstmt = connection.prepareStatement(sql);
+
+			pstmt.setInt(1, userID);
+
+			ResultSet resultSet = pstmt.executeQuery();
+
+			resultSet.close();
+			connection.close();
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+
+	public User searchUser(String mailAddress)throws SQLException{
+		Connection connection;
+		User user = new User();
+		String sql = "select * from user where mailAddress=?";
+
+		try{
+			Class.forName(driverClassName);
+			connection = DriverManager.getConnection(url, username, password);
+			PreparedStatement pstmt = connection.prepareStatement(sql);
+
+			pstmt.setString(1, mailAddress);
+
+			ResultSet resultSet = pstmt.executeQuery();
+
+			int userid = resultSet.getInt("userID");
+			String userName = resultSet.getString("username");
+			String password = resultSet.getString("password");
+			String profile = resultSet.getString("profile");
+			Byte icon = resultSet.getByte("icon");
+			ArrayList<Integer> MIDI_IDs = (ArrayList<Integer>)resultSet.getObject("MIDI_IDs");
+			ArrayList<Integer> comment_IDs = (ArrayList<Integer>) resultSet.getObject("commentIDs");
+			Boolean isManagaer = resultSet.getBoolean("isManager");
+
+			user.setUserID(userid);
+			user.setUsername(userName);
+			user.setPassword(password);
+			user.setMailAddress(mailAddress);
+			user.setProfile(profile);
+			user.setIcon(icon);
+			user.setMIDI_IDs(MIDI_IDs);
+			user.setCommentIDs(comment_IDs);
+			user.setManager(isManagaer);
+
+			resultSet.close();
+			connection.close();
+
+			return user;
+		}catch(Exception e){
+			e.printStackTrace();
+
+			return null;
+		}
+	}
+
+
+	public int searchNoUserID()throws SQLException{
+		Connection connection;
+		String sql = "select * from user where userID=?";
+
+		try{
+			Class.forName(driverClassName);
+			connection = DriverManager.getConnection(url, username, password);
+			PreparedStatement pstmt = connection.prepareStatement(sql);
+			ResultSet resultSet;
+
+			int i=1;
+			int id;
+
+			while(true){
+				pstmt.setInt(1,i );
+				resultSet = pstmt.executeQuery();
+
+				id = resultSet.getInt("userID");
+				if(id==0){
+					break;
+				}
+				i++;
+			}
+			resultSet.close();
+			connection.close();
+			return i;
+
+		}catch(Exception e){
+			e.printStackTrace();
+			return 0;
+		}
+	}
+
+
 }
